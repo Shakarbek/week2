60d777c6ca9ea032d2ecfcf873ff78922fda08f4
Shakarbek <47443293+Shakarbek@users.noreply.github.com>
2019-02-15 09:14:37 +0600

    Update Program.cs

--- task/Program.cs
+++ task/Program.cs
@@ -14,12 +14,12 @@ namespace task4
             string path = @"C:\Users\user\Desktop\tup - tup 2\Lab2\MyTest.txt";
             string path2 = @"C:\Users\user\Desktop\tup - tup 2\ghj\MyTest.txt";
           
-                if (!File.Exists(path)) 
+                if (!File.Exists(path)) //если файл не существует
                 {
-                FileStream fs = File.Create(path);
+                FileStream fs = File.Create(path);//создаем этот файл
                 }
 
-            File.Move(path, path2);
+            File.Move(path, path2);//переместим через Move файл в другую папку, учитывая, что в первой папке он исчезает
         }
     }
 }
